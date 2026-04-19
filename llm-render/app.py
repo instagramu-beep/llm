6291da45a68512a2808740e985c9c5b61fb5349b
Prince Kumar verma <shinghsakshi59@gmail.com>
2026-04-20 04:16:51 +0530

    Update app.py

--- llm-render/app.py
+++ llm-render/app.py
@@ -36,6 +36,9 @@
 #         "prompt": prompt,
 #         "response": output["choices"][0]["text"]
 #     }
+
+
+
 from fastapi import FastAPI
 from llama_cpp import Llama
 from huggingface_hub import hf_hub_download
@@ -76,8 +79,82 @@ def generate(prompt: str):
         temperature=0.7,
         stop=["<|end|>", "<|user|>"]
     )
-
-    return {
+   return {
         "prompt": prompt,
         "response": output["choices"][0]["text"].strip()
     }
+
+
+
+
+
+# from fastapi import FastAPI
+# from llama_cpp import Llama
+# from huggingface_hub import hf_hub_download
+# import threading
+
+# app = FastAPI()
+
+# llm = None
+# model_loaded = False
+
+# def load_model():
+#     global llm, model_loaded
+
+#     print("Downloading model...")
+
+#     model_path = hf_hub_download(
+#         repo_id="TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF",
+#         filename="tinyllama-1.1b-chat-v1.0.Q2_K.gguf"
+#     )
+
+#     print("Loading model into memory...")
+
+#     llm = Llama(
+#         model_path=model_path,
+#         n_ctx=128,
+#         n_threads=1,
+#         n_batch=16
+#     )
+
+#     model_loaded = True
+#     print("Model loaded successfully!")
+
+# @app.get("/")
+# def home():
+#     return {
+#         "status": "running",
+#         "model_loaded": model_loaded
+#     }
+
+# @app.get("/warmup")
+# def warmup():
+#     global model_loaded
+
+#     if not model_loaded:
+#         threading.Thread(target=load_model).start()
+#         return {"status": "model loading started"}
+#     return {"status": "already loaded"}
+
+# @app.get("/generate")
+# def generate(prompt: str):
+#     global llm, model_loaded
+
+#     if not model_loaded:
+#         return {"error": "Model still loading. Call /warmup first."}
+
+#     formatted_prompt = f"<|user|>\n{prompt}\n<|assistant|>\n"
+
+#     output = llm(
+#         formatted_prompt,
+#         max_tokens=40,
+#         temperature=0.7
+#     )
+
+#     return {
+#         "response": output["choices"][0]["text"].strip()
+#     }
+# #     return {
+# #         "prompt": prompt,
+#         "response": output["choices"][0]["text"].strip()
+#     }
